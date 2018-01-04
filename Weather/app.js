48344634ce4dbbcd9d74fded5604e4244f1fbfdd
Jared Zuñiga <jaredhz2597@outlook.com>
2018-01-04 12:29:45 -0600

    weather app included

--- Weather/app.js
+++ Weather/app.js
@@ -1,8 +1,32 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand : true,
+            alias : 'address',
+            describe : 'Address to fetch weather for',
+            string : true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+var address = argv.a;
+
+address = encodeURIComponent(address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+address,
     json: true
 },(error,response,body)=>{
-    console.log(body);
-});
+    (error===true) ? console.log("Unable to connect to google servers") : console.log("Coordenates:");
+    if(body.status==='ZERO_RESULTS')
+    {
+        console.log("Address not found");
+    }else if(body.status === 'OK'){
+        console.log("Adress: "+ body.results[0].formatted_address);
+        console.log("Latitude: "+body.results[0].geometry.location.lat+" Longitude: "+body.results[0].geometry.location.lng);
+    }
+})
